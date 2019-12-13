f40259a9f999f9a2992dc882a1d26a700514adc7
Nhan Nguyen <nxtnhan@gmail.com>
2019-12-13 18:26:17 +0900

    import Vietnamese

--- resources/assets/js/plugins/i18n.js
+++ resources/assets/js/plugins/i18n.js
@@ -1,18 +1,20 @@
-import Vue from 'vue'
-import VueI18n from 'vue-i18n'
-import en from './en.json'
-import fr from './fr.json'
-import es from './es.json'
+import Vue from "vue";
+import VueI18n from "vue-i18n";
+import en from "./en.json";
+import fr from "./fr.json";
+import es from "./es.json";
+import vi from "./vi.json";
 
-Vue.use(VueI18n)
+Vue.use(VueI18n);
 
 const i18n = new VueI18n({
-  locale: 'en',
+  locale: "en",
   messages: {
     en,
     fr,
-    es
+    es,
+    vi
   }
-})
+});
 
-export default i18n
+export default i18n;
